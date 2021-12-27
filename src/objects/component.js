c03de3617ff9fedaa3121b84ec075ad606de4509
Styro457 <raduursache6@gmail.com>
2021-12-27 17:53:14 +0200

    Created Component class

---
+++ src/objects/component.js
@@ -0,0 +1,14 @@
+/**
+ * @classdesc
+ * Base class for everything attached to objects.
+ * @class Component
+ * @constructor
+ * @param {object} properties Component properties
+ */
+class Component {
+
+    constructor(properties) {
+        this.properties = properties;
+    }
+
+}
