b6222950b415da36471baea82d9cdb474872377e
Styro457 <raduursache6@gmail.com>
2021-12-27 21:30:57 +0200

    Created sprite-renderer.js

---
+++ src/objects/image/sprite-renderer.js
@@ -0,0 +1,25 @@
+import Renderer from "../render/renderer.js";
+import Game from "../../game.js";
+
+/**
+ * @classdesc
+ * Component for rendering sprites
+ * @class SpriteRenderer
+ * @constructor
+ * @param {object} properties Component properties
+ * @param {Texture} properties.texture The texture of the sprite
+ * @param {Object} properties.parent The object the component is part of
+ */
+class SpriteRenderer extends Renderer {
+
+    constructor(properties) {
+        super(properties);
+    }
+
+    onUpdate() {
+        Game.instance.options.renderEngine.drawImage(this.properties.texture, this.properties.parent.properties.position, this.properties.parent.properties.rotation, this.properties.parent.properties.scale);
+    }
+
+}
+
+export default SpriteRenderer;
