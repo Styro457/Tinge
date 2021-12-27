a31305aed8932164a5bf0f6376cb31ad760ba862
Styro457 <raduursache6@gmail.com>
2021-12-27 23:44:09 +0200

    Created Rectangle

---
+++ src/math/shapes/rectangle.js
@@ -0,0 +1,33 @@
+class Rectangle {
+
+    constructor(x, y, width, height) {
+        /**
+         * The x coordinate of the rectangles bottom left corner
+         * @name x
+         * @type {number}
+         */
+        this.x = x;
+
+        /**
+         * The y coordinate of the rectangles bottom left corner
+         * @name y
+         * @type {number}
+         */
+        this.y = y;
+
+        /**
+         * The width of the rectangle
+         * @name width
+         * @type {number}
+         */
+        this.width = width;
+
+        /**
+         * The height of the rectangle
+         * @name height
+         * @type {number}
+         */
+        this.height = height;
+    }
+
+}
