48824c86dccb7e0c6a9707e6f08c995438d9cd0a
Styro457 <raduursache6@gmail.com>
2021-12-27 23:44:14 +0200

    Created Circle

---
+++ src/math/shapes/circle.js
@@ -0,0 +1,26 @@
+class Circle {
+
+    constructor(x, y, radius) {
+        /**
+         * The x coordinate of the circles center
+         * @name x
+         * @type {number}
+         */
+        this.x = x;
+
+        /**
+         * The y coordinate of the circles center
+         * @name y
+         * @type {number}
+         */
+        this.y = y;
+
+        /**
+         * The radius of the circle
+         * @name radius
+         * @type {number}
+         */
+        this.radius = radius;
+    }
+
+}
