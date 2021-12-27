0f43d699cc5bfd7c4cd2bc0175af28b504f60d7e
Styro457 <raduursache6@gmail.com>
2021-12-27 17:53:07 +0200

    Created Object class

---
+++ src/objects/object.js
@@ -0,0 +1,15 @@
+/**
+ * @classdesc
+ * Base class for objects.
+ * @class Object
+ * @constructor
+ * @param {object} options Object properties
+ */
+class Object {
+
+    constructor(properties) {
+        this.properties = properties;
+        this.components = [];
+    }
+
+}
