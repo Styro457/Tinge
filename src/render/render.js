a0cbc09ec14d4c9eb390d119bcc335be9754f637
Styro457 <raduursache6@gmail.com>
2021-12-27 21:32:24 +0200

    Updated render.js

--- src/render/render.js
+++ src/render/render.js
@@ -1,35 +1,56 @@
 /**
  * @classdesc
- * a base renderer object
- * @class Renderer
+ * a base render object
+ * @class Render
  * @constructor
  * @param {object} options The renderer parameters
  * @param {number} options.width The width of the canvas without scaling
  * @param {number} options.height The height of the canvas without scaling
  * @param {HTMLCanvasElement} [options.canvas] The html canvas to draw to on screen
  */
-class RenderEngine {
+class Render {
 
     constructor(options) {
-
+        this.options = options;
     }
 
     /**
-     * clear the canvas
+     * Clear the canvas
      * @name clear
      * @function
      */
     clear() {}
 
     /**
-     * return a reference to the screen canvas
+     * Return a reference to the screen canvas
      * @name getCanvas
      * @function
      * @returns {HTMLCanvasElement}
      */
-    getCanvas() {}
-
-    drawImage() {
-
+    getCanvas() {
+        return this.options.canvas;
     }
+
+    /**
+     * Return a reference to the screen canvas context
+     * @name getCanvas
+     * @function
+     * @returns {CanvasRenderingContext2D | WebGLRenderingContext}
+     */
+    getContext() {
+        return this.options.context;
+    }
+
+    /**
+     * Draw an image on the canvas
+     * @name clear
+     * @function
+     * @param {Texture} texture The image source
+     * @param {Vector} position The position of the image on the screen
+     * @param {Vector} rotation The rotation of the image
+     * @param {Vector} scale The scale of the image
+     */
+    drawImage(texture, position, rotation, scale) {}
 }
+
+export default Render;
