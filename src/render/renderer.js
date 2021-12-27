9123cac73d31101fdcae22f9eebdd5af56b46d63
Styro457 <raduursache6@gmail.com>
2021-12-27 17:27:26 +0200

    Created renderer

---
+++ src/render/renderer.js
@@ -0,0 +1,31 @@
+/**
+ * @classdesc
+ * a base renderer object
+ * @class Renderer
+ * @constructor
+ * @param {object} options The renderer parameters
+ * @param {number} options.width The width of the canvas without scaling
+ * @param {number} options.height The height of the canvas without scaling
+ * @param {HTMLCanvasElement} [options.canvas] The html canvas to draw to on screen
+ */
+class Renderer {
+
+    constructor(options) {
+
+    }
+
+    /**
+     * clear the canvas
+     * @name clear
+     * @function
+     */
+    clear() {}
+
+    /**
+     * return a reference to the screen canvas
+     * @name getCanvas
+     * @function
+     * @returns {HTMLCanvasElement}
+     */
+    getCanvas() {}
+}
