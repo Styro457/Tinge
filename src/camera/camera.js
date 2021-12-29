4e4fead674f4305c72311509022179c7909c7b8a
Styro457 <raduursache6@gmail.com>
2021-12-29 02:34:34 +0200

    Created Camera class

---
+++ src/camera/camera.js
@@ -0,0 +1,12 @@
+import Object from "../objects/object.js";
+
+/**
+ * Camera object
+ * @class Camera
+ * @constructor
+ * @param {object} properties Object properties
+ */
+class Camera extends Object {
+}
+
+export default Camera;
