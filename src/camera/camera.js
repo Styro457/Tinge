6a977c7c47459ae2dbb58ba4bbdf39f93d59808a
Styro457 <raduursache6@gmail.com>
2021-12-29 02:34:44 +0200

    Added worldToScreenPosition function

--- src/camera/camera.js
+++ src/camera/camera.js
@@ -1,4 +1,6 @@
 import Object from "../objects/object.js";
+import Renderer from "../render/renderer.js";
+import Vector from "../math/vector.js";
 
 /**
  * Camera object
@@ -7,6 +9,18 @@ import Object from "../objects/object.js";
  * @param {object} properties Object properties
  */
 class Camera extends Object {
+
+    /**
+     * Returns the position on the screen of a point in the world
+     * @name worldToScreenPosition
+     * @function
+     * @param {Vector} worldPosition The position in the world
+     * @returns {Vector} The position on the screen
+     */
+    static worldToScreenPosition(worldPosition) {
+        return worldPosition.clone().subtractV(Renderer.instance.mainCamera.getPosition()).addV(new Vector(Renderer.instance.getWidth()/2, Renderer.instance.getHeight()/2));
+    }
+
 }
 
 export default Camera;
